b2068e129a1cb74144e05889a73c5f80e5134b1e
Honor L <lvrong_cn@163.com>
2019-07-30 00:18:34 +0800

    User authentication with Flask-login

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask,url_for,render_template,redirect,flash
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import StringField,SubmitField,PasswordField
 
 from werkzeug.security import generate_password_hash,check_password_hash
+
+from flask_login import LoginManager,UserMixin,login_user,login_required,logout_user,current_user
 
 import os
 
@@ -18,7 +20,7 @@ app.config['SECRET_KEY'] = 'secret strings'
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(20))
     username = db.Column(db.String(20))
@@ -50,6 +52,24 @@ class EditMovie(FlaskForm):
 class DeleteMovie(FlaskForm):
     submit = SubmitField('Delete')
 
+class Settings(FlaskForm):
+    name = StringField('name')
+    submit = SubmitField('Update')
+
+class Login_Form(FlaskForm):
+    username = StringField('username')
+    password = PasswordField('password')
+    submit = SubmitField('submit')
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+ 
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+
 @app.context_processor
 def inject_user():
     user = User.query.first()
@@ -73,7 +93,40 @@ def index():
 
     return render_template('index.html' ,form = form,form_delete = form_delete ,movies = movies)
 
+@app.route('/login',methods = ['GET','POST'])
+def login():
+    
+    form  = Login_Form()
+
+    if form.validate_on_submit():
+
+        username = form.username.data
+        password = form.password.data
+
+        if not username or not password:
+            flash('Invalid input!')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+
+        if username == user.username and user.validate_password(password):
+            login_user(user)
+            flash('login success')
+            return redirect(url_for('index'))
+
+        flash('invalid username or password')
+        return redirect(url_for('login'))
+    return render_template('login.html',form = form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Goodbye.')
+    return redirect(url_for('index'))
+
 @app.route('/movie/edit/<movie_id>',methods = ['GET','POST'])
+@login_required
 def edit(movie_id):
     form = EditMovie()
 
@@ -90,14 +143,36 @@ def edit(movie_id):
 
     return render_template('edit.html',form = form,movie = movie )
 
+
 @app.route('/movie/delete/<movie_id>',methods = ['POST'])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
     db.session.commit()
+    flash('Item deleted.')
 
     return redirect(url_for('index'))
 
+@app.route('/settings',methods = ['GET','POST'])
+@login_required
+def settings():
+    form = Settings()
+    if form.validate_on_submit():
+        name = form.name.data
+        flash(name)
+
+        if not name :
+            flash('Invali input.')
+            return redirect(url_for('settings'))
+        current_user.name = name
+        #user = User.query.first()
+        #user.name = name
+
+        db.session.commit()
+        flash('Settings Updated.')
+        return redirect(url_for('index'))
+    return render_template('settings.html', form = form)
 
 @app.errorhandler(404)
 def page_not_found(e):
